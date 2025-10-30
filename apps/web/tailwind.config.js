9be0c79f76aaa63ce38d7fe3a19639a60dddc324
AnamSadat <anamsadat3@gmail.com>
2025-10-31 06:12:32 +0700

    chore: setup tailwindcss config in monorepo

---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,10 @@
+import sharedConfig from "@repo/tailwind-config/tailwind.config.js";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  ...sharedConfig,
+  // content: [
+  //   "./src/**/*.{js,ts,jsx,tsx}",
+  //   "../../packages/ui/src/**/*.{js,ts,jsx,tsx}",
+  // ],
+};
---
+++ packages/tailwind-config/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  darkMode: ["class"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1E40AF",
+        secondary: "#9333EA",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
