2dadd26a684ee5d5f97a58e5ac6f9308878399e8
AnamSadat <anamsadat3@gmail.com>
2025-10-31 13:39:27 +0700

    chore: setup shadcn/ui config in monorepo

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,10 +1,11 @@
-import sharedConfig from "@repo/tailwind-config/tailwind.config.js";
+// import sharedConfig from "../../packages/tailwind-config/tailwind.config.js";
+import sharedConfig from "@repo/tailwind-config/tailwind-config";
 
 /** @type {import('tailwindcss').Config} */
 export default {
   ...sharedConfig,
-  // content: [
-  //   "./src/**/*.{js,ts,jsx,tsx}",
-  //   "../../packages/ui/src/**/*.{js,ts,jsx,tsx}",
-  // ],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "../../packages/ui/src/**/*.{js,ts,jsx,tsx}",
+  ],
 };
--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -1,14 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 const config = {
-  darkMode: ["class"],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#1E40AF",
-        secondary: "#9333EA",
-      },
-    },
-  },
   plugins: [],
 };
 
