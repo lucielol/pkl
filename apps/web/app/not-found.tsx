a991ec99bf41e1d791730610afdecc26fc3d0cd2
AnamSadat <anamsadat3@gmail.com>
2025-11-02 14:20:07 +0700

    refactor: update not-found style

--- apps/web/app/not-found.tsx
+++ apps/web/app/not-found.tsx
@@ -1,7 +1,32 @@
+"use client";
+
+import { Button } from "@repo/ui/components/button";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 export default function NotFound() {
+  const router = useRouter();
   return (
-    <div>
-      <h1>Page Not Found</h1>
+    <div className="flex h-screen flex-col items-center justify-center bg-gray-50 dark:bg-background text-center">
+      <h1 className="text-6xl font-extrabold text-blue-600 mb-4">404</h1>
+      <h2 className="text-2xl font-semibold mb-2 text-gray-800 dark:text-white">
+        Oops! Halaman Tidak Ditemukan
+      </h2>
+      <p className="text-gray-400 mb-6">
+        {"Silakan periksa kembali URL Anda atau kembali ke "}
+        <Link
+          href="/"
+          className="underline bg-transparent underline-offset-1 cursor-pointer transition-colors duration-200 text-gray-400 hover:text-white"
+        >
+          halaman utama.
+        </Link>
+      </p>
+      <Button
+        onClick={() => router.back()}
+        className="cursor-pointer bg-blue-500 hover:bg-blue-700 transition-colors duration-200 text-white"
+      >
+        Kembali
+      </Button>
     </div>
   );
 }
