37c8ce0381574db13dd502261e5665aab4a5bb63
AnamSadat <anamsadat3@gmail.com>
2025-11-07 23:55:49 +0700

    refactor: fix bug style in signup

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,27 +1,34 @@
 "use client";
 
 import { AppFormSignIn, AppFormSignUp } from "@/components/organism";
+import { cn } from "@repo/ui/utils";
 import Image from "next/image";
 import { useState } from "react";
 
 export default function AuthPage() {
   const [mode, setMode] = useState<"signIn" | "signUp">("signIn");
-
   const isSignIn = mode === "signIn";
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-50 dark:bg-background">
-      <div className="sm:flex md:grid grid-cols-2 w-4/5 max-w-6xl h-full md:min-h-[600px] rounded-2xl overflow-hidden shadow-2xl dark:shadow-gray-900">
+    <div
+      className={cn(
+        "flex min-h-screen bg-gray-50 dark:bg-background",
+        isSignIn
+          ? "justify-center items-center"
+          : "justify-center items-start pt-16 pb-14"
+      )}
+    >
+      <div className="sm:flex md:grid grid-cols-2 w-4/5 max-w-6xl md:min-h-[600px] rounded-2xl overflow-hidden shadow-2xl dark:shadow-gray-900">
         {/* Kiri */}
         <div className="flex flex-col justify-center gap-5 px-10 pt-10 md:pt-0 pb-12 bg-white dark:bg-sidebar">
-          <div className="flex justify-between items-center">
-            <h1 className="text-2xl font-semibold ">LOGO</h1>
+          <div className="flex justify-between items-center pt-10">
+            <h1 className="text-2xl font-semibold">LOGO</h1>
             <p className="text-sm hidden md:block">
               {isSignIn ? "Belum punya akun? " : "Sudah punya akun? "}
               <button
                 type="button"
                 onClick={() => setMode(isSignIn ? "signUp" : "signIn")}
-                className="text-blue-500 hover:underline hover:underline-offset-2 ml-1"
+                className="text-blue-500 hover:underline hover:underline-offset-2 ml-1 cursor-pointer"
               >
                 {isSignIn ? "Sign Up" : "Sign In"}
               </button>
@@ -35,7 +42,7 @@ export default function AuthPage() {
             <button
               type="button"
               onClick={() => setMode(isSignIn ? "signUp" : "signIn")}
-              className="text-blue-500 hover:underline hover:underline-offset-2 ml-1"
+              className="text-blue-500 hover:underline hover:underline-offset-2 ml-1 cursor-pointer"
             >
               {isSignIn ? "Sign Up" : "Sign In"}
             </button>
